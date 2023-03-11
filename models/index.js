2c5408c47f47bec0acc2cfb8808320a09bcdae1a
agtTwilight <lukasmacmillen@gmail.com>
2023-03-11 15:20:50 -0800

    editing db as needed to fit front end needs

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Company = require('./Company');
 const Item = require('./Item');
-const Image = require('./Image')
 
 
-module.exports = { User, Company, Item, Image };
+module.exports = { User, Company, Item };
