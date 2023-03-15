308e1a1a17e73dfd9007a2c7e16f761a700bb45a
Binh Ngo <bnngo97@gmail.com>
2023-03-15 14:48:32 -0700

    created company and user seeds

--- utils/seedData.js
+++ utils/seedData.js
@@ -13,6 +13,75 @@ const demoU2 = {
         address: "1234 N street",
 }
 
+const demoU3 = {
+        username: "Pedro Diaz",
+        email: "pedro@email.com",
+        password: "password",
+        address: "1234 Bishop Street",
+}
+
+const demoU4 = {
+        username: "Jason Nguyen",
+        email: "jason@jason.com",
+        password: "password",
+        address: "24 H. Fitness Street",
+}
+
+const demoU5 = {
+        username: "Bram Gibson",
+        email: "bram@email.com",
+        password: "password",
+        address: "21 Jump Street",
+}
+
+const demoU6 = {
+        username: "Evan McKinnon",
+        email: "evan@email.com",
+        password: "password",
+        address: "1234 Rainier Avenue",
+}
+
+const demoU7 = {
+        username: "Dante Freeman",
+        email: "dante@email.com",
+        password: "password",
+        address: "1234 Chicago Street",
+}
+
+const demoU8 = {
+        username: "Michael Chen",
+        email: "michael@email.com",
+        password: "password",
+        address: "1234 S street",
+}
+
+const demoU9 = {
+        username: "Fiona Walters",
+        email: "fiona@email.com",
+        password: "password",
+        address: "1234 N street",
+}
+
+const demoU10 = {
+        username: "Mabel Ly",
+        email: "mabel@email.com",
+        password: "password",
+        address: "1234 N street",
+}
+
+const demoU11 = {
+        username: "Richard Hertz",
+        email: "richard@email.com",
+        password: "password",
+        address: "1234 N street",
+}
+
+const demoU12 = {
+        username: "Andy James",
+        email: "andy@email.com",
+        password: "password",
+        address: "1234 N street",
+}
 // company seeds
 const demoC1 = {
         lookingForWork: true,
@@ -26,6 +95,76 @@ const demoC2 = {
         description: "Best Breakfast In Town!",
         tags: ["Breakfast", "Light", "Simple"],
         username: "Lukas"
+}
+
+const demoC3 = {
+        lookingForWork: true,
+        description: "Previous professional chef making comfort Mexican food.",
+        tags: ["Mexican", "Seafood"],
+        username: "Pedro Diaz"
+}
+
+const demoC4 = {
+        lookingForWork: true,
+        description: "Meal Prep plans and healthy meals!",
+        tags: ["Vegetarian", "Asian", "Noodles"],
+        username: "Jason Nguyen"
+}
+
+const demoC5 = {
+        lookingForWork: true,
+        description: "I am a baker that also makes Italian-American foods!",
+        tags: ["Sweets", "Italian", "American"],
+        username: "Bram Gibson"
+}
+
+const demoC6 = {
+        lookingForWork: true,
+        description: "Ex-Sushi Chef that loves to make Japanese food!",
+        tags: ["Asian", "Noodles", "Japanese"],
+        username: "Evan McKinnon"
+}
+
+const demoC7 = {
+        lookingForWork: true,
+        description: "Comforting breakfast cook",
+        tags: ["Breakfast", "American", "Sweets"],
+        username: "Dante Freeman"
+}
+
+const demoC8 = {
+        lookingForWork: true,
+        description: "Health conscious dishes for healthy people",
+        tags: ["Vegan", "Vegetarian", "American"],
+        username: "Michael Chen"
+}
+
+const demoC9 = {
+        lookingForWork: true,
+        description: "I love making pasta and other noodle dishes!",
+        tags: ["Noodles", "Italian", "Japanese"],
+        username: "Fiona Walters"
+}
+
+const demoC10 = {
+        lookingForWork: true,
+        description: "Retired professional cook",
+        tags: ["American", "Asian", "Mexican", "Sweets"],
+        username: "Mabel Ly"
+}
+
+const demoC11 = {
+        lookingForWork: true,
+        description: "Healthy food made with local ingredients.",
+        tags: ["Vegetarian", "Italian", "American"],
+        username: "Richard Hertz"
+}
+
+const demoC12 = {
+        lookingForWork: true,
+        description: "Culinary instructor that loves to cook for people.",
+        tags: ["Mexican", "Seafood", "Asian", "Japanese"],
+        username: "Andy James"
 }
 
 // item seeds
@@ -47,4 +186,6 @@ const demoI3 = {
         description: "Avocado toast topped with nutritional yeast, salt, pepper, cayenne, and one over medium egg."
 }
 
-module.exports = {demoU1, demoU2, demoC1, demoC2, demoI1, demoI2, demoI3}
+module.exports = {demoU1, demoU2, demoU3,demoU4,demoU5,demoU6,demoU7,demoU8,demoU9,demoU10,demoU11,demoU12,
+                  demoC1, demoC2, demoC3,demoC4,demoC5,demoC6,demoC7,demoC8,demoC9,demoC10,demoC11,demoC12,
+                  demoI1, demoI2, demoI3}
